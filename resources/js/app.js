96e2f493c87b7be80aafb320816c8e03426548ca
nguyen.thanh <nguyen.thanh@re-fine.jp>
2023-06-09 14:43:28 +0700

    init

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = createApp({});
+
+app.component('example-component', ExampleComponent);
+
+app.mount('#app');
