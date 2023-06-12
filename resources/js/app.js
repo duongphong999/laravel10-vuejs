1aa264fe9fc4a1df8813cc1d993905a0d1c6b4c2
nguyen.thanh <nguyen.thanh@re-fine.jp>
2023-06-12 13:30:27 +0700

    - Split layout admin and import bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@
  */
 
 import './bootstrap';
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap-utilities.min.css";
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container-fluid">
+        <div class="row align-items-center" style="background-color: #0c713d; padding: 1rem;">
+            <div class="col-12">
+                HEADER
+            </div>
+        </div>
+    </div>
+</template>
