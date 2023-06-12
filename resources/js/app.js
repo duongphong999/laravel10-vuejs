0bd7c97acd9e9031214e371483a14da1afde633f
nguyen.thanh <nguyen.thanh@re-fine.jp>
2023-06-12 11:06:42 +0700

    Create router vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,9 @@
 
 import './bootstrap';
 import { createApp } from 'vue';
-import ExampleComponent from './components/ExampleComponent.vue';
+import App from './App.vue';
+import router from './router';
 
-const app = createApp({});
-
-app.component('example-component', ExampleComponent);
-
+const app = createApp(App);
+app.use(router);
 app.mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*')->name('laravue');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('user');
